206d0b31e8216af4c9a65dd61d84a168c5feec9b
TropinDmitry <85580920+TropinDmitry@users.noreply.github.com>
2024-10-29 22:31:54 +0700

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+/*const TelegramBot = require('node-telegram-bot-api');
+
+const token = '7747217090:AAGrLncvc0J_cDh3Z2RHZl8LrPQ7terbUMo';
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});*/
