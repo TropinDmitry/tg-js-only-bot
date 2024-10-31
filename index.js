ee60e884f097aed49b61c30318081a1e0c9a8bc3
TropinDmitry <85580920+TropinDmitry@users.noreply.github.com>
2024-10-31 21:45:34 +0700

    test2

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '7747217090:AAGrLncvc0J_cDh3Z2RHZl8LrPQ7terbUMo';
+const token = '7867171176:AAGxTX9V3F25vyN1g9Ue5cM7GmlAp5VzdJQ';
+const webAppUrl = 'https://elaborate-maamoul-89293c.netlify.app/';
 
 const bot = new TelegramBot(token, {polling: true});
 
-bot.on('message', (msg) => {
+bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
+  const text = msg.text;
+
+  if(text === '/start') {
+    await bot.sendMessage(chatId, 'Заполните форму', {
+      reply_markup: {
+        keyboard: [
+          [{text: 'Заполнить форму', web_app: {url: webAppUrl}}]
+        ]
+      }
+    })
+
+    await bot.sendMessage(chatId, 'Перейти в магазин', {
+      reply_markup: {
+        inline_keyboard: [
+          [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
+        ]
+      }
+    })
+  }
 
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+  //bot.sendMessage(chatId, 'Received your message');
 });
